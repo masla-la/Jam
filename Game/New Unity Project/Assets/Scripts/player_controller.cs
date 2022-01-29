0bcfd36e506a1129d43b72416bf1a95b517e33d9
event <event@c2r8s4.42urduliz.com>
2022-01-29 13:18:10 +0100

    nuevo

--- Game/New Unity Project/Assets/Scripts/player_controller.cs
+++ Game/New Unity Project/Assets/Scripts/player_controller.cs
@@ -7,24 +7,44 @@ public class player_controller : MonoBehaviour
     public float speed = 1000.0f;
     public float jumpforce = 25.0f;
     int canjump = 0;
-
+    Vector3 originalPos;
+    Vector2 movement;
     private Rigidbody2D _body;
 
     void Start() 
     {
+            originalPos = transform.position; // get a copy
+             
+             originalPos.x = Mathf.RoundToInt(gameObject.transform.position.x);
+             originalPos.y = Mathf.RoundToInt(gameObject.transform.position.y);
+             originalPos.z = Mathf.RoundToInt(gameObject.transform.position.z);
+             
+             transform.position = originalPos;
         _body = GetComponent<Rigidbody2D>();
     }
 
     void Update() 
     {
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        if (deltaX > 50)
+            movement = new Vector2(50, _body.velocity.y);
+        else if (deltaX < -50)
+            movement = new Vector2(-50, _body.velocity.y);
+        else
+            movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
 
         if (Input.GetKeyDown(KeyCode.W) && canjump == 1)
         {
             canjump = 0;
             _body.AddForce (Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
+        
+        if (gameObject.transform.position.y < -24)
+        {
+            gameObject.transform.position = originalPos;
+            movement = new Vector2(0, 0);
+            _body.velocity = movement;
         }
     }
 
